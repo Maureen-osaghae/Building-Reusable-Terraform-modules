15851f24ed29a3003a1c5d5c620e1a395aaf9490
Maureen Osaghae <maureenosaghae86@gmail.com>
2025-08-13 16:01:33 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,4 +10,36 @@ terraform {
 provider "aws" {
   # Configuration options
   region = var.aws_region
+
 }
+
+
+#Past this in the root directory module main.tf file
+
+module "ec2_instance" {
+  source        = "./modules/ec2"
+  instance_type = "t2.micro"
+  image_os      = "ubuntu"
+  tagging = {
+    "name" = "maureen-instance"
+    "BusinessUnit" = "infosec"
+  }
+}
+
+#Root directory module variable .tf file
+
+variable "aws_region" {
+    type = string
+    description = "Region to deploy our resources"
+    default = "us-east-1"
+  
+}
+
+variable "tagging" {
+  type = map(string)
+  default = {
+    name         = "maureen-instance"
+    BusinessUnit = "infosec"
+  }
+}
+  
