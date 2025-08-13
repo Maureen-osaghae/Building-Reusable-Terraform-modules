28661fac656f83d13544772a2767618dfb4db22a
Maureen Osaghae <maureenosaghae86@gmail.com>
2025-08-13 15:51:53 +0100

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,9 +1,104 @@
-module "ec2_instance" {
-  source        = "./modules/ec2"
-  instance_type = "t2.micro"
-  image_os      = "ubuntu"
-  tagging = {
-    "name" = "maureen-instance"
-    "BusinessUnit" = "infosec"
+resource "aws_instance" "Ec2_instance" {
+  ami           = local.image
+  instance_type = var.instance_type
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.example.id]
+  root_block_device {
+    encrypted = true
   }
+  tags = var.tagging
+}
+  
+locals {
+  ubuntu = var.image_os == "ubuntu" ? data.aws_ami.ubuntu_server_latest.id : null
+  amazon = var.image_os == "amazon linux" ? data.aws_ami.amazon_linux_latest.id : null
+  image  = coalesce(local.ubuntu, local.amazon)
+}
+
+data "aws_ami" "amazon_linux_latest" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["*al2023-ami-2023*x86_64*"]
+  }
+  filter {
+    name   = "platform-details"
+    values = ["Linux/UNIX"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami" "ubuntu_server_latest" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["*ubuntu-jammy-22.04-amd64-server*"]
+  }
+  filter {
+    name   = "platform-details"
+    values = ["Linux/UNIX"]
+  }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+# Custom VPC
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Get available AZs
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  filtered_azs = [for az in data.aws_availability_zones.available.names : az if contains(["us-east-1f", "us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"], az)]
+}
+
+# Subnet within the VPC
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.custom_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = element(local.filtered_azs, 0)
+}
+
+# Security group
+resource "aws_security_group" "example" {
+  name        = "Example Security Group"
+  description = "Example security group for EC2 instance"
+  vpc_id      = aws_vpc.custom_vpc.id
+}
+
+# Ingress rule
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.example.id
+
+  cidr_ipv4   = aws_vpc.custom_vpc.cidr_block
+  from_port   = 80
+  to_port     = 80
+  ip_protocol = "tcp"
+}
+
+# Egress rule
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.example.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  ip_protocol = "-1"
 }
--- variables.tf
+++ variables.tf
@@ -1,15 +1,32 @@
-variable "aws_region" {
+variable "instance_type" {
     type = string
-    description = "Region to deploy our resources"
-    default = "us-east-1"
-  
+    description = "You have option to select t2.micro or t3.micro"
+    
+    validation {
+      condition     = (var.instance_type == "t2.micro" || var.instance_type == "t3.micro")
+    error_message = "The instance_type must be 't2.micro' or 't3.micro'"
+  }
+    
 }
 
-variable "tagging" {
-  type = map(string)
-  default = {
-    name         = "maureen-instance"
-    BusinessUnit = "infosec"
+  variable "tagging" {
+    type = map(string)
+    description = "add tage BusinessUnit or other tags"
+
+    validation {
+        condition = contains(keys(var.tagging), "BusinessUnit")
+        error_message = "At least one tag must be called 'BusinessUnit'"
+    }
+    
+  }
+
+  variable "image_os" {
+  type        = string
+  description = "Select the image OS. Options 'ubuntu' or 'amazon linux'"
+
+  validation {
+    condition     = (var.image_os == "ubuntu" || var.image_os == "amazon linux")
+    error_message = "The image_os must be 'ubuntu' or 'amazon linux'"
   }
 }
-  
+
